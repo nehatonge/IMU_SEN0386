e912bf840fd1789d716f9dfa1f7cb187f750e60b
nehatonge <nehatonge30@gmail.com>
2026-02-24 17:12:57 +0530

    Refactor setup.py for package discovery and description
    Updated package setup to use find_packages() and modified data_files installation paths. Simplified the package description.

--- src/imu_serial_node/setup.py
+++ src/imu_serial_node/setup.py
@@ -1,22 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from glob import glob
+import os
 
 package_name = 'imu_serial_node'
 
 setup(
     name=package_name,
     version='1.0.0',
-    packages=[package_name],
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-
-        # 🔥 Install launch files
-        ('share/' + package_name + '/launch', glob('launch/*.py')),
-
-        # 🔥 Install config files
-        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/launch',
+            glob(os.path.join('launch', '*.py'))),
+        ('share/' + package_name + '/config',
+            glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=[
         'setuptools',
@@ -25,7 +24,7 @@ setup(
     zip_safe=True,
     maintainer='Neha Tonge',
     maintainer_email='nehatonge30@gmail.com',
-    description='ROS2 driver for DFRobot Serial 6-Axis IMU (SEN0386) with auto + manual multi-IMU support.',
+    description='ROS2 driver for DFRobot Serial 6-Axis IMU (SEN0386)',
     license='All rights reserved',
     tests_require=['pytest'],
     entry_points={
