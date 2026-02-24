805f23e4bbc46480d5a94a232da017fd449ad183
nehatonge <nehatonge30@gmail.com>
2026-02-24 13:49:15 +0530

    Update setup.py for versioning and dependencies
    Updated package version, added pyserial dependency, and improved package metadata.

--- src/imu_serial_node/setup.py
+++ src/imu_serial_node/setup.py
@@ -1,25 +1,31 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'imu_serial_node'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='1.0.0',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'pyserial',
+    ],
     zip_safe=True,
-    maintainer='neh',
-    maintainer_email='neh@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Neha Tonge',
+    maintainer_email='nehatonge30@gmail.com',
+    description='ROS2 driver for DFRobot Serial 6-Axis IMU (SEN0386) with automatic USB port detection.',
+    license='All rights reserved',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': ['imu_node = imu_serial_node.imu_node:main',
+        'console_scripts': [
+            'imu_node = imu_serial_node.imu_node:main',
         ],
     },
 )
